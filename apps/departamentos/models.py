ac6a3143e05daecb327aa55feea0de16fa4e8f3b
Python <fgpasso@gmail.com>
2019-09-14 10:48:10 -0300

    Criando a app departamentos

---
+++ apps/departamentos/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Departamento(models.Model):
+    nome = models.CharField(max_length=50, help_text='Nome do departamento')
+
+    def __str__(self):
+        return self.nome
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.empresas',
-    'apps.colaboradores'
+    'apps.colaboradores',
+    'apps.departamentos',
 ]
 
 MIDDLEWARE = [
