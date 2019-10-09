74ed4cc15df3a77dba5f909183e77d32737f7256
Python <fgpasso@gmail.com>
2019-10-09 12:24:15 -0300

    inclusão da rotina de exportar para excel.

--- apps/horas_extra/urls.py
+++ apps/horas_extra/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import (HorasExtraList,
-                    HorasExtraCreate,
-                    HorasExtraUpdate,
-                    HorasExtraColaboradorUpdate,
-                    HorasExtraDelete,
-                    HorasExtraUse,
-                    )
+from .views import (
+    HorasExtraList,
+    HorasExtraCreate,
+    HorasExtraUpdate,
+    HorasExtraColaboradorUpdate,
+    HorasExtraDelete,
+    HorasExtraUse,
+    HorasExtraExportCSV,
+    HorasExtraExportXLS,
+)
 
 
 urlpatterns = [
@@ -15,4 +18,6 @@ urlpatterns = [
     path('Editar-colaborador/<int:pk>', HorasExtraColaboradorUpdate.as_view(), name='update_horasextra_colaborador'),
     path('utilizar-horasextra/<int:pk>', HorasExtraUse.as_view(), name='use_horasextra'),
     path('excluir/<int:pk>', HorasExtraDelete.as_view(), name='delete_horasextra'),
+    path('exportar-csv', HorasExtraExportCSV.as_view(), name='export_horasextra_csv'),
+    path('exportar-xls', HorasExtraExportXLS.as_view(), name='export_horasextra_xls'),
 ]
--- apps/horas_extra/views.py
+++ apps/horas_extra/views.py
@@ -1,4 +1,6 @@
+import csv
 import json
+import xlwt
 
 from django.http import HttpResponse
 from django.urls import reverse_lazy
@@ -75,3 +77,62 @@ class HorasExtraUse(View):
             })
 
         return HttpResponse(response, content_type='application/json')
+
+
+class HorasExtraExportCSV(View):
+    def get(self, request):
+        response = HttpResponse(content_type='text/csv')
+        response['Content_Disposition'] = 'attachament; filename="somefilename.csv'
+
+        horasextras = HorasExtra.objects.filter(utilizada=False)
+
+        writer = csv.writer(response)
+        writer.writerow(['Id', 'Motivo', 'Colaborador', 'Rest. Colab.', 'Horas'])
+
+        for horaextra in horasextras:
+            writer.writerow(
+                [horaextra.id, horaextra.motivo,
+                 horaextra.colaborador,
+                 horaextra.colaborador.total_horasextra,
+                 horaextra.horas
+            ])
+
+        return response
+
+
+class HorasExtraExportXLS(View):
+    def get(self, request):
+        response = HttpResponse(content_type='application/ms-excel')
+        response['Content_Disposition'] = 'attachament; filename="users.xls'
+
+        wb = xlwt.Workbook(encoding='utf-8')
+        ws = wb.add_sheet('Users')
+
+        row_num = 0
+
+        font_style = xlwt.XFStyle()
+        font_style.font.bold = True
+
+        columns = ['Id', 'Motivo', 'Colaborador', 'Rest. Colab.', 'Horas']
+
+        for col_num in range(len(columns)):
+            ws.write(row_num, col_num, columns[col_num], font_style)
+
+        font_style = xlwt.XFStyle()
+
+        horasextras = HorasExtra.objects.filter(utilizada=False)
+
+        row_num = 1
+
+        for horaextra in horasextras:
+            ws.write(row_num, 0, horaextra.id, font_style)
+            ws.write(row_num, 1, horaextra.motivo, font_style)
+            ws.write(row_num, 2, horaextra.colaborador.nome, font_style)
+            ws.write(row_num, 3, horaextra.colaborador.total_horasextra, font_style)
+            ws.write(row_num, 4, horaextra.horas, font_style)
+
+            row_num += 1
+
+        wb.save(response)
+
+        return response
